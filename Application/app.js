54a79240cc8e3ed73d668d9042f4e17c013cc4ed
belcaid abderrahim <belcaidna@gmail.com>
2020-03-31 15:47:04 +0100

    Add login page

--- Application/app.js
+++ Application/app.js
@@ -3,15 +3,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const path = require('path');
+
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.get('/', function(req, res){
+    res.sendFile(__dirname + "/login.html");
+});
+app.get('/register', function(req, res){
+    res.sendFile(__dirname + "/register.html");
+});
+
+app.get('/Salaire', function(req, res){
     res.sendFile(__dirname + "/index.html");
 });
 
-app.post('/',function(req, res){
+app.post('/Salaire',function(req, res){
     var name = req.body.name;
     var name2 = req.body.name2;
     var age = req.body.age;
@@ -42,6 +54,73 @@ fs.readFile('data.json', 'utf-8', function (err, data) {
 });
 
 app.use("/", express.static(__dirname + "/"));
+
+
+
+// Register validation  ------------------------
+app.post('/register',function(req, res){
+    var name = req.body.name;
+    var mail = req.body.mail;
+    var password = req.body.password;
+    var confirPassword = req.body.confirPassword;
+
+
+fs.readFile('./users.json', 'utf-8', function(err, data) {
+	if (err) throw err
+
+	var arrayOfObjects = JSON.parse(data);
+	arrayOfObjects.push({
+		name: name,
+        mail: mail,
+        password : password,
+        confirPassword : confirPassword
+	});
+
+    // console.log(arrayOfObjects);
+    
+    fs.writeFile('./users.json', JSON.stringify(arrayOfObjects), 'utf-8', function(err) {
+        if (err) throw err;
+        console.log('Done!');
+        res.sendFile(__dirname + "/page1.html");
+
+        
+    });
+});
+    
+});
+// ---------------------------------------------
+// Register validation  ------------------------
+app.post('/',function(req, res){
+    var name = req.body.name;
+    var password = req.body.password;
+
+
+fs.readFile('./users.json', 'utf-8', function(err, data) {
+	if (err) throw err
+
+	var arrayOfObjects = JSON.parse(data);
+	
+
+    console.log(arrayOfObjects);
+
+    arrayOfObjects.forEach(element => {
+        if (name === element.name && password === element.password ) {
+
+         res.sendFile(__dirname + "/page1.html");
+
+        }else
+        res.sendFile(__dirname + "/404.html");
+
+        
+    
+        
+    });
+    
+});
+    
+})
+// --------------------------------------------
+
 
 app.listen('3000',function(){
     console.log("Server listning on port 3000...");
