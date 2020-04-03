8accecb228b2b0f32c8333a9aeb660a828f0ebd0
belcaid abderrahim <belcaidna@gmail.com>
2020-04-03 19:52:58 +0100

    Add to app.js

--- Application/app.js
+++ Application/app.js
@@ -93,3 +93,219 @@ fs.writeFile('./data/Entreprise.json',JSON.stringify(list),(err)=>{
 resp.render('pages/Page1',{entreprise:list});
 });
 app.listen(3320);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // ------------------------------------------------------------------------------------------- hennnna 
+// /*jshint esversion: 6 */
+
+// const express = require('express');
+// const bodyParser = require('body-parser')
+// const fs = require('fs');
+// const uuid = require('uuid');
+// const path = require('path');
+
+
+// const app = express();
+// app.use(bodyParser.urlencoded({extended: true}));
+
+
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.get('/', function(req, res){
+//     res.sendFile(__dirname + "/login.html");
+// });
+// app.get('/register', function(req, res){
+//     res.sendFile(__dirname + "/register.html");
+// });
+
+// app.get('/Salaire', function(req, res){
+//     res.sendFile(__dirname + "/index.html");
+// });
+// app.get('/contactus', function (req, res) {
+//     res.sendFile(__dirname + "/formpage.html");
+// });
+
+// app.post('/contactus', function (req, res) {
+//     var message = req.body.message;
+//     var name = req.body.name;
+//     var email = req.body.email;
+    
+
+
+//     fs.readFile('contactus.json', 'utf-8', function (err, data) {
+//         if (err) throw err;
+
+//         var arrayOfObjects = JSON.parse(data);
+//         arrayOfObjects.companies.push({
+//             message: message,
+//             name: name,
+//             email: email,
+//         });
+
+//         console.log(arrayOfObjects);
+
+//         fs.writeFile('contactus.json', JSON.stringify(arrayOfObjects), 'utf-8', function (err) {
+//             if (err) throw err;
+//             console.log('Done!');
+//             res.sendFile(__dirname + "/formpage.html");
+
+//         });
+//     });
+
+// })
+
+// app.post('/Salaire',function(req, res){
+//     var name = req.body.name;
+//     var name2 = req.body.name2;
+//     var age = req.body.age;
+//     var slr = req.body.slr;
+
+
+// fs.readFile('data.json', 'utf-8', function (err, data) {
+// 	if (err) throw err;
+
+// 	var arrayOfObjects = JSON.parse(data);
+// 	arrayOfObjects.companies.push({
+//         matricule : uuid.v4(),
+// 		name: name,
+//         name2: name2,
+//         age: age,
+//         slr: slr
+// 	});
+
+//     console.log(arrayOfObjects);
+    
+//     fs.writeFile('data.json', JSON.stringify(arrayOfObjects), 'utf-8', function(err) {
+//         if (err) throw err;
+//         console.log('Done!');
+//         res.sendFile(__dirname + "/index.html");
+  
+//     });
+// });
+    
+// });
+
+// app.use("/", express.static(__dirname + "/"));
+
+
+
+// // Register validation  ------------------------
+// app.post('/register',function(req, res){
+//     var name = req.body.name;
+//     var mail = req.body.mail;
+//     var password = req.body.password;
+//     var confirPassword = req.body.confirPassword;
+
+
+// fs.readFile('./users.json', 'utf-8', function(err, data) {
+// 	if (err) throw err
+
+// 	var arrayOfObjects = JSON.parse(data);
+// 	arrayOfObjects.push({
+// 		name: name,
+//         mail: mail,
+//         password : password,
+//         confirPassword : confirPassword
+// 	});
+
+//     // console.log(arrayOfObjects);
+    
+//     fs.writeFile('./users.json', JSON.stringify(arrayOfObjects), 'utf-8', function(err) {
+//         if (err) throw err;
+//         console.log('Done!');
+//         res.sendFile(__dirname + "/page1.html");
+
+        
+//     });
+// });
+    
+// });
+
+// app.get('/userss',function(req, res){
+
+//     fs.readFile('./data.json', 'utf-8', function(err, data) {
+//         if (err) throw err;
+    
+//         var arrayOfObjects = JSON.parse(data);
+      
+//         res.send(arrayOfObjects);
+//         console.log(arrayOfObjects);
+        
+//     });
+// })
+// app.get('/contactusdata', function (req, res) {
+
+//     fs.readFile('./contactus.json', 'utf-8', function (err, data) {
+//         if (err) throw err;
+
+//         var arrayOfObjects = JSON.parse(data);
+
+//         res.send(arrayOfObjects);
+//         console.log(arrayOfObjects);
+
+//     });
+// })
+
+
+
+
+// // ---------------------------------------------
+// // Register validation  ------------------------
+// app.post('/',function(req, res){
+//     var name = req.body.name;
+//     var password = req.body.password;
+
+
+// fs.readFile('./users.json', 'utf-8', function(err, data) {
+// 	if (err) throw err
+
+// 	var arrayOfObjects = JSON.parse(data);
+	
+
+//     console.log(arrayOfObjects);
+
+//     arrayOfObjects.forEach(element => {
+//         if (name === element.name && password === element.password ) {
+
+//          res.sendFile(__dirname + "/page1.html");
+
+//         }else
+//         res.sendFile(__dirname + "/404.html");
+
+        
+    
+        
+//     });
+    
+// });
+    
+// })
+// // --------------------------------------------
+
+
+
+
+
+
+    
+
+
+
+
+// app.listen('3000',function(){
+//     console.log("Server listning on port 3000...");
+    
+// });
+// // --------------------------------------------------------------------------------------------
